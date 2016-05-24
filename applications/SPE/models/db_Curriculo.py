674402d7b2d19085f66666ea41ad8980af1f969f
aadelop <10-10195@usb.ve>
2016-05-24 19:07:29 -0400

    Eliminadas tablas en plural (Materiales, Objetivos)// Actualizado db_Curriculo.py

--- applications/SPE/models/db_Curriculo.py
+++ applications/SPE/models/db_Curriculo.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
 
-################################################################################
-#                         INICIO DECLARACION BASE DE DATOS                     #
-################################################################################
-
-#------------------------------------------------------------------------------#
-#                            MODULO DE INVENTARIO                              #
-#------------------------------------------------------------------------------#
 
 db.define_table('Curriculo',
-    Field('nombre', unique=True,
-          requires=[IS_NOT_EMPTY(error_message='Introduzca un nombre.'),
-                    IS_NOT_IN_DB(db, 'Usuario.nombre', error_message='Nombre ya\
-                    esta almacenado, introduzca otro o modifique el anterior.'),
-                    IS_UPPER()], label='Nombre (*)' )
+    Field('electivas','string'),
+    Field('cursos','string'),
+    Field('aficiones','string'),
+    Field('idiomas','string')
    )
 
-#------------------------------------------------------------------------------#
-
-################################################################################
-#                          FIN DECLARACION BASE DE DATOS                       #
-################################################################################
--- applications/SPE/models/db_Objetivos.py
+++
@@ -1,6 +0,0 @@
-db.define_table('Objetivo',
-    Field('descripcion','text',
-          label= 'Descripcion'),
-    Field('actividad',##referencia 1 a muchos tabla Actividad
-          label='Actividades')
-)
