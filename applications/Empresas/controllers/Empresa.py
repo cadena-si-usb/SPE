2e3288b9f304a350982a7db504dec7c788768d8f
franciscosucre <frank91frank@gmail.com>
2016-05-25 18:19:52 -0430

    (FJSG) Integrado antiguo registrar empresa

--- applications/Empresas/controllers/Empresa.py
+++ applications/Empresas/controllers/Empresa.py
@@ -3,20 +3,20 @@
 # Proceso de registro de Empresa por medio de la opcion Empresa -> Registrarse, en el Index
 def registrar_Empresa():
     # Agregamos los campos en el orden deseado, comenzamos con el login y el password
-    fields = [db.UsuarioExterno.correo,db.UsuarioExterno.password]
+    fields = [db.UsuarioExterno.correo,db.UsuarioExterno.clave]
     # Agregamos un campo extra de comfirm password el cual debera tener el mismo valor que el password para ser aceptado
     fields += [Field('comfirm_Password','password', label=T('Comfirm Password'),
                      requires = [IS_EXPR('value==%s' % repr(request.vars.password),error_message=T('Las contraseñas no coinciden'))])]
     # Agregamos el resto de los campos
     fields += [
         db.UsuarioExterno.pregunta_secreta,
-        db.UsuarioExterno.respuesta_pregunta_secreta,
+        db.UsuarioExterno.respuesta_secreta,
         db.UsuarioExterno.nombre,
-        db.UsuarioExterno.id_pais,
-        db.UsuarioExterno.id_estado,
-        db.Empresa.id_area_laboral,
+        db.UsuarioExterno.pais,
+        db.UsuarioExterno.estado,
+        db.Empresa.area_laboral,
         db.UsuarioExterno.direccion,
-        db.Empresa.pag_web,
+        db.Empresa.direccion_web,
         db.Empresa.descripcion,
         db.UsuarioExterno.telefono,
         db.Empresa.contacto_RRHH
@@ -29,14 +29,14 @@ def registrar_Empresa():
     separator=': ',
     buttons=['submit'],
     col3 = {'log':T('Identificación de acceso unica asignada a la Empresa'),
-            'password':T('Contraseña para acceder al sistema'),
+            'clave':T('Contraseña para acceder al sistema'),
             'comfirm_Password':T('Repita su contraseña'),
             'pregunta_secreta':T('Si necesita obtener de nuevo su contraseña se le hara esta pregunta'),
-            'respuesta_pregunta_secreta':T('Respuesta a su pregunta secreta'),
+            'respuesta_secreta':T('Respuesta a su pregunta secreta'),
             'nombre':T('Nombre comercial de la Empresa'),
-            'id_pais':T('Pais en el que se encuentra la Empresa'),
-            'id_estado':T('Estado del pais en el que se encuentra'),
-            'id_area_laboral':T('Area Laboral de la Empresa'),
+            'pais':T('Pais en el que se encuentra la Empresa'),
+            'estado':T('Estado del pais en el que se encuentra'),
+            'area_laboral':T('Area Laboral de la Empresa'),
             'direccion':T('Direccion de las instalaciones de la Empresa'),
             'pag_web':T('Pagina Web de la Empresa'),
             'descripcion':T('Descripcion breve de la Empresa, su vision y sus funciones'),
@@ -48,13 +48,13 @@ def registrar_Empresa():
     if form.process().accepted:
         # Registramos la Empresa
         db.Empresa.insert(log = request.vars.log,
-                             password = request.vars.password,
+                            clave = request.vars.password,
                              pregunta_secreta = request.vars.pregunta_secreta,
-                             respuesta_pregunta_secreta = request.vars.respuesta_pregunta_secreta,
+                             respuesta_pregunta_secreta = request.vars.respuesta_secreta,
                              nombre = request.vars.nombre,
-                             id_pais = request.vars.id_pais,
-                             id_estado = request.vars.id_estado,
-                             id_area_laboral = request.vars.id_area_laboral,
+                             pais = request.vars.pais,
+                             estado = request.vars.estado,
+                             area_laboral = request.vars.area_laboral,
                              direccion = request.vars.direccion,
                              pag_web = request.vars.pag_web,
                              descripcion = request.vars.descripcion,
@@ -72,13 +72,13 @@ def registrar_Empresa():
 
         generar_Correo_Verificacion(request.vars.log)
 
-        paisSet = db(db.pais.id == request.vars.id_pais).select()
+        paisSet = db(db.pais.id == request.vars.pais).select()
         pais = paisSet[0].nombre
 
-        estadoSet = db(db.estado.id == request.vars.id_estado).select()
+        estadoSet = db(db.estado.id == request.vars.estado).select()
         estado = estadoSet[0].nombre
 
-        arealaboralSet = db(db.area_laboral.id == request.vars.id_area_laboral).select()
+        arealaboralSet = db(db.area_laboral.id == request.vars.area_laboral).select()
         area_laboral = arealaboralSet[0].nombre
 
         # Mensaje de exito
@@ -97,7 +97,7 @@ def registrar_Empresa():
                                telefono=request.vars.telefono,
                                contacto_RRHH=request.vars.contacto_RRHH,
                                pregunta_secreta=request.vars.pregunta_secreta,
-                               respuesta_pregunta_secreta=request.vars.respuesta_pregunta_secreta)
+                               respuesta_pregunta_secreta=request.vars.respuesta_secreta)
     # Caso 2: El form no se lleno de manera correcta asi que recargamos la pagina
     else:
         return response.render('Empresa/registrarEmpresa/registrar_Empresa.html',message=T("Registrar Empresa"),form=form)
@@ -110,7 +110,7 @@ def registrar_Tutor_Industrial():
         db.Tutor_Industrial.nombre,
         db.Tutor_Industrial.apellido,
         db.Tutor_Industrial.ci,
-        db.Tutor_Industrial.password
+        db.Tutor_Industrial.clave
     ]
     # Agregamos un campo extra de comfirm password el cual debera tener el mismo valor que el password para ser aceptado
     fields += [Field('comfirm_Password','password', label=T('Comfirm Password'),
@@ -118,14 +118,14 @@ def registrar_Tutor_Industrial():
     # Agregamos el resto de los campos
     fields +=[
         db.Tutor_Industrial.pregunta_secreta,
-        db.Tutor_Industrial.respuesta_pregunta_secreta,
+        db.Tutor_Industrial.respuesta_secreta,
         db.Tutor_Industrial.profesion,
         db.Tutor_Industrial.cargo,
         db.Tutor_Industrial.departamento,
         db.Tutor_Industrial.direccion,
-        db.Tutor_Industrial.id_pais,
-        db.Tutor_Industrial.id_estado,
-        db.Tutor_Industrial.id_universidad,
+        db.Tutor_Industrial.pais,
+        db.Tutor_Industrial.estado,
+        db.Tutor_Industrial.universidad,
         db.Tutor_Industrial.telefono
     ]
 
@@ -149,9 +149,9 @@ def registrar_Tutor_Industrial():
             'cargo':T('Cargo que ocupa en la Empresa'),
             'departamento':T('Departamento de la Empresa en la que trabaja'),
             'direccion':T('Direccion del tutor industrial'),
-            'id_pais':T('Pais en el que se encuentra domiciliado el tutor industrial'),
-            'id_estado':T('Estado en el que se encuentra domiciliado el tutor industrial'),
-            'id_universidad':T('Universidad de la cual egreso el tutor'),
+            'pais':T('Pais en el que se encuentra domiciliado el tutor industrial'),
+            'estado':T('Estado en el que se encuentra domiciliado el tutor industrial'),
+            'universidad':T('Universidad de la cual egreso el tutor'),
             'telefono':T('Numerico telefonico del tutor industrial')
            })
     # Caso 1: El form se lleno de manera correcta asi que registramos al tutor y procedemos a la pagina de exito
@@ -168,14 +168,14 @@ def registrar_Tutor_Industrial():
             password = request.vars.password,
             pregunta_secreta = request.vars.pregunta_secreta,
             respuesta_pregunta_secreta = request.vars.respuesta_pregunta_secreta,
-            id_Empresa = EmpresaRegistradora.id,
+            Empresa = EmpresaRegistradora.id,
             profesion = request.vars.profesion,
             cargo = request.vars.cargo,
             departamento = request.vars.departamento,
             direccion = request.vars.direccion,
-            id_pais = request.vars.id_pais,
-            id_estado = request.vars.id_estado,
-            id_universidad = request.vars.id_universidad,
+            pais = request.vars.pais,
+            estado = request.vars.estado,
+            universidad = request.vars.universidad,
             telefono = request.vars.telefono)
 
         #Insertamos en la tabla user de Web2py
@@ -190,13 +190,13 @@ def registrar_Tutor_Industrial():
 
         generar_Correo_Verificacion(request.vars.email)
 
-        paisSet = db(db.pais.id == request.vars.id_pais).select()
+        paisSet = db(db.pais.id == request.vars.pais).select()
         pais = paisSet[0].nombre
 
-        estadoSet = db(db.estado.id == request.vars.id_estado).select()
+        estadoSet = db(db.estado.id == request.vars.estado).select()
         estado = estadoSet[0].nombre
 
-        universidadSet = db(db.universidad.id == request.vars.id_universidad).select()
+        universidadSet = db(db.universidad.id == request.vars.universidad).select()
         universidad = universidadSet[0].nombre
 
         # Mensaje de exito
