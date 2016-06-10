b96402189cca3bc852b4e7b19f7d78234a99d8de
Hector Goncalves <hectoragoncalvesp@gmail.com>
2016-06-09 23:16:34 -0400

    Vista de editar pasantia lista, empezando a implementar planes_trabajo

--- applications/SPE/models/db_5_Plan_Trabajo.py
+++ applications/SPE/models/db_5_Plan_Trabajo.py
@@ -4,14 +4,10 @@
 db.define_table('Plan_Trabajo',
     Field('pasantia','reference Pasantia',
           label='Pasantia' ),
-    Field('objetivo_general','string',
-          label='Obejtivos Especificos'),
     Field('aprobacion_tutor_academico', 'integer', default=0, requires=IS_INT_IN_RANGE(minimum=0, maximum=1),
           label='Obejtivos Especificos'),
     Field('aprobacion_Tutor_Industrial', 'integer', default=0,requires=IS_INT_IN_RANGE(minimum=0, maximum=1),
           label='Obejtivos Especificos'),
     Field('aprobacion_coordinacion', 'integer', default=0, requires=IS_INT_IN_RANGE(minimum=0, maximum=1),
           label='Obejtivos Especificos'),
-    Field('objetivo_general', 'string',
-          label='Obejtivos Especificos')
 )
