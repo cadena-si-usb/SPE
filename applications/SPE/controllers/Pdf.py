2198737ab984c09365c4d3820aa6a50bfaf1e6f5
robertoromero8892 <robertoromero8892@gmail.com>
2016-10-12 08:58:42 -0400

    Imprimir pdf

--- applications/SPE/controllers/Pdf.py
+++ applications/SPE/controllers/Pdf.py
@@ -1,42 +1,117 @@
 # -*- coding: utf-8 -*-
 
 import cStringIO
-
+from reportlab.lib.enums import TA_CENTER, TA_LEFT
 from reportlab.pdfgen import canvas
 from reportlab.platypus.doctemplate import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, Spacer, Table
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
+from reportlab.lib.colors import HexColor, purple, yellow, black, blue
 
+
+def stylesheet():
+    styles = {
+        'default': ParagraphStyle(
+            'default',
+            fontName='Times-Roman',
+            fontSize=10,
+            leading=10,
+            leftIndent=0,
+            rightIndent=0,
+            firstLineIndent=0,
+            alignment=TA_LEFT,
+            spaceBefore=0,
+            spaceAfter=0,
+            bulletFontName='Times-Roman',
+            bulletFontSize=10,
+            bulletIndent=0,
+            textColor=black,
+            backColor=None,
+            wordWrap=None,
+            borderWidth=0,
+            borderPadding=0,
+            borderColor=None,
+            borderRadius=None,
+            allowWidows=1,
+            allowOrphans=0,
+            textTransform=None,  # 'uppercase' | 'lowercase' | None
+            endDots=None,
+            splitLongWords=1,
+        ),
+    }
+    styles['bluetitle'] = ParagraphStyle(
+        'bluetitle',
+        parent=styles['default'],
+        fontName='Helvetica-Bold',
+        fontSize=11,
+        leading=10,
+        alignment=TA_LEFT,
+        textColor=blue,
+    )
+    styles['blacktitle'] = ParagraphStyle(
+        'blacktitle',
+        parent=styles['default'],
+        fontName='Helvetica-Bold',
+        fontSize=9,
+        leading=10,
+        alignment=TA_LEFT,
+        textColor=black,
+    )
+    return styles
 
 
 def generarPdfConstanciaCulminacion():
-    """
-    To display the generated PDF in your browser go to:
-     http://.../<app>/<controller>/generate
+    # Datos del estudiante
 
-    To download it as hello.pdf, for example, instead, use:
-     http://.../<app>/<controller>/generate/hello.pdf
-    """
+    userid = session.currentUser.id
+    currentUser = db.UsuarioUSB(db.UsuarioUSB.id == userid)
+    rol = db(
+        (db.auth_membership.user_id == userid) & (db.auth_membership.group_id == db.auth_group.id)).select().first()
+    usuario = {
+        "apellido": currentUser.apellido,
+        "nombre": currentUser.nombre,
+        "rol": rol.auth_group.role,
+    }
 
-    styles = getSampleStyleSheet()
+    estudiante = db(((db.UsuarioUSB.id == userid) & (db.Estudiante.usuario == db.UsuarioUSB.id))).select().first()
+    carrera = db.Carrera(id=estudiante.Estudiante.carrera)
+    sede = db(db.Sede.id == db.Estudiante.sede).select().first()
+    pasantia = db(db.Pasantia.estudiante == estudiante.Estudiante).select().first()
+
+    styles = stylesheet()
+
     story = [
-        Paragraph("Hello World", styles['Heading1']),
-        Paragraph("The quick brown fox", styles['Normal']),
-        Spacer(1, 0.25*inch),
-        Paragraph("jumps over the lazy dog.", styles['Normal'])]
+        Paragraph("ESTUDIANTE", styles['bluetitle']),
+    ]
+
+    #no separar en parrafos poner tod pegado
+    tbl_data = [
+        [Paragraph("Nombre y Apellido:" + str(usuario['nombre']) + str(usuario['apellido']), styles["default"]),
+         Paragraph("Carnet:" + str(estudiante.Estudiante.carnet), styles["default"]),
+         Paragraph("Cedula:" + str(estudiante.UsuarioUSB.numero_documento), styles["default"])
+         ],
+        [Paragraph("Carrera: " + str(carrera.nombre), styles["default"]),
+         Paragraph("Telefono:" + str(estudiante.UsuarioUSB.telefono), styles["default"]),
+         Paragraph("Email:" + str(estudiante.UsuarioUSB.correo), styles["default"])
+         ],
+        [Paragraph("Periodo de Pasantia:" + str(pasantia.titulo) + str(pasantia.periodo), styles["default"]),
+         ],
+
+    ]
+    tbl = Table(tbl_data)
+    story.append(tbl)
+
     buffer = cStringIO.StringIO()
     doc = SimpleDocTemplate(buffer)
     doc.build(story)
     pdf = buffer.getvalue()
     buffer.close()
 
-    filename = request.args(0)
-    if filename:
-        header = {'Content-Disposition': 'attachment; filename=' + filename}
-    else:
-        header = {'Content-Type': 'application/pdf'}
+    filename = "Plan de Trabajo"
+    header = {'Content-Disposition': 'attachment; filename=' + filename}
     response.headers.update(header)
+
     return pdf
 
-
+    # Spacer(1, 0.25 * inch),
