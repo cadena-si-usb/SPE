bb782d9e5567fbd22cb5afa26d9911e18b1cfcb6
robertoromero8892 <robertoromero8892@gmail.com>
2016-10-19 11:06:56 -0400

    front pasantias

--- applications/SPE/controllers/pasantia_etapas.py
+++ applications/SPE/controllers/pasantia_etapas.py
@@ -4,10 +4,15 @@ import Encoder
 
 Etapa = Etapa()
 
-def etapas():
-    obj = Encoder.to_dict(request.vars)
 
+def etapas():
+    userid = session.currentUser.id
+    currentUser = db.UsuarioUSB(db.UsuarioUSB.id == userid)
+    estudiante = db(((db.UsuarioUSB.id == userid) & (db.Estudiante.usuario == db.UsuarioUSB.id))).select().first()
+    pasantia = db(db.Pasantia.estudiante == estudiante.Estudiante).select().first()
+
+    obj = Encoder.to_dict(request.vars)
     rows = Etapa.find(obj)
 
     response.view = 'mis_pasantias/etapas.load.html'
-    return dict(etapas=rows.as_list(),id="id")
+    return dict(etapas=rows.as_list(), id="id", pasantia=pasantia)
