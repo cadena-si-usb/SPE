705dfb77d79a4c7e6a1b8bcc983f2ae2f58b8f81
Stefani Castellanos <scct95@hotmail.com>
2017-02-04 12:21:40 -0400

    un primer intento de arreglar el controlador del sidebar

--- applications/SPE/controllers/sidebar.py
+++ applications/SPE/controllers/sidebar.py
@@ -5,6 +5,61 @@ import Encoder
 
 Accion_Usuario = Accion_Usuario()
 
+def sidebar():
+
+    obj = Encoder.to_dict(request.vars)
+    if (auth.is_logged_in()):
+        usuario = auth.user
+        roles = db(db.auth_membership.user_id == auth.user.id).select()
+        roles_id = []
+        rows = []
+        sidebar = []
+        for rol in roles:
+            roles_id.append(rol.group_id)
+            names = db(db.auth_group.id == rol.group_id.id).select(db.auth_group.role)
+            # aux = []
+            # for name in names:
+            #     aux.append(name.role)
+
+        for name in names:
+            contexto = []
+            # verificar cuando haya un rol multiperfil
+
+            rows = db((db.Accion_Usuario.rol.belongs(roles_id)) & (db.Accion_Usuario.contexto ==
+                                                                'coordinacion')).select(
+                orderby=db.Accion_Usuario.first_name)
+            accion = []
+            for row in rows:
+                accion.append(row.first_name)
+            contexto.append(['coordinacion', accion])
+
+            rows = db((db.Accion_Usuario.rol.belongs(roles_id)) & (db.Accion_Usuario.contexto ==
+                                                                'pasantias')).select(
+                orderby=db.Accion_Usuario.first_name)
+            accion = []
+            for row in rows:
+                accion.append(row.first_name)
+            contexto.append(['pasantias', accion])
+
+            rows = db((db.Accion_Usuario.rol.belongs(roles_id)) & (db.Accion_Usuario.contexto ==
+                                                                'catalogos')).select(
+                orderby=db.Accion_Usuario.first_name)
+            accion = []
+            for row in rows:
+                accion.append(row.first_name)
+            contexto.append(['catalogos', accion])
+
+            rows = db((db.Accion_Usuario.rol.belongs(roles_id)) & (db.Accion_Usuario.contexto ==
+                                                                'configuracion')).select(
+                orderby=db.Accion_Usuario.first_name)
+            accion = []
+            for row in rows:
+                accion.append(row.first_name)
+            contexto.append(['configuracion', accion])
+            sidebar.append([name.role, contexto])
+
+    response.view = 'sidebar/sidebar.html'
+    return dict(routes=rows, sidebar=sidebar)
 
 def coordinacion():
     if (auth.is_logged_in()):
